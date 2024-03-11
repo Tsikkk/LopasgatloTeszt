9a2bd85e53faee25629599b95689fc891f5c4e3f
unknown <kantor.krisztian20@egressy.info>
2024-03-11 14:01:14 +0100

    anyad

---
+++ app/Quiz/Quiz/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Quiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.kerdes = new System.Windows.Forms.Label();
+            this.valasz1 = new System.Windows.Forms.Label();
+            this.valasz2 = new System.Windows.Forms.Label();
+            this.valasz3 = new System.Windows.Forms.Label();
+            this.ckeckAnsw = new System.Windows.Forms.Button();
+            this.next = new System.Windows.Forms.Button();
+            this.valasz1C = new System.Windows.Forms.RadioButton();
+            this.valasz2C = new System.Windows.Forms.RadioButton();
+            this.valasz3C = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // kerdes
+            // 
+            this.kerdes.AutoSize = true;
+            this.kerdes.Location = new System.Drawing.Point(31, 46);
+            this.kerdes.Name = "kerdes";
+            this.kerdes.Size = new System.Drawing.Size(49, 16);
+            this.kerdes.TabIndex = 0;
+            this.kerdes.Text = "kérdés";
+            // 
+            // valasz1
+            // 
+            this.valasz1.AutoSize = true;
+            this.valasz1.Location = new System.Drawing.Point(173, 94);
+            this.valasz1.Name = "valasz1";
+            this.valasz1.Size = new System.Drawing.Size(53, 16);
+            this.valasz1.TabIndex = 1;
+            this.valasz1.Text = "valasz1";
+            // 
+            // valasz2
+            // 
+            this.valasz2.AutoSize = true;
+            this.valasz2.Location = new System.Drawing.Point(173, 141);
+            this.valasz2.Name = "valasz2";
+            this.valasz2.Size = new System.Drawing.Size(53, 16);
+            this.valasz2.TabIndex = 2;
+            this.valasz2.Text = "valasz2";
+            // 
+            // valasz3
+            // 
+            this.valasz3.AutoSize = true;
+            this.valasz3.Location = new System.Drawing.Point(173, 186);
+            this.valasz3.Name = "valasz3";
+            this.valasz3.Size = new System.Drawing.Size(53, 16);
+            this.valasz3.TabIndex = 3;
+            this.valasz3.Text = "valasz3";
+            // 
+            // ckeckAnsw
+            // 
+            this.ckeckAnsw.Location = new System.Drawing.Point(67, 347);
+            this.ckeckAnsw.Name = "ckeckAnsw";
+            this.ckeckAnsw.Size = new System.Drawing.Size(221, 23);
+            this.ckeckAnsw.TabIndex = 9;
+            this.ckeckAnsw.Text = "Helyes válaszok megtekintése";
+            this.ckeckAnsw.UseVisualStyleBackColor = true;
+            this.ckeckAnsw.Click += new System.EventHandler(this.ckeckAnsw_Click);
+            // 
+            // next
+            // 
+            this.next.Location = new System.Drawing.Point(322, 347);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(126, 23);
+            this.next.TabIndex = 10;
+            this.next.Text = "Következő kérdés";
+            this.next.UseVisualStyleBackColor = true;
+            this.next.Click += new System.EventHandler(this.next_Click_1);
+            // 
+            // valasz1C
+            // 
+            this.valasz1C.AutoSize = true;
+            this.valasz1C.Location = new System.Drawing.Point(134, 94);
+            this.valasz1C.Name = "valasz1C";
+            this.valasz1C.Size = new System.Drawing.Size(17, 16);
+            this.valasz1C.TabIndex = 11;
+            this.valasz1C.TabStop = true;
+            this.valasz1C.UseVisualStyleBackColor = true;
+            // 
+            // valasz2C
+            // 
+            this.valasz2C.AutoSize = true;
+            this.valasz2C.Location = new System.Drawing.Point(134, 141);
+            this.valasz2C.Name = "valasz2C";
+            this.valasz2C.Size = new System.Drawing.Size(17, 16);
+            this.valasz2C.TabIndex = 12;
+            this.valasz2C.TabStop = true;
+            this.valasz2C.UseVisualStyleBackColor = true;
+            // 
+            // valasz3C
+            // 
+            this.valasz3C.AutoSize = true;
+            this.valasz3C.Location = new System.Drawing.Point(134, 186);
+            this.valasz3C.Name = "valasz3C";
+            this.valasz3C.Size = new System.Drawing.Size(17, 16);
+            this.valasz3C.TabIndex = 13;
+            this.valasz3C.TabStop = true;
+            this.valasz3C.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.valasz3C);
+            this.Controls.Add(this.valasz2C);
+            this.Controls.Add(this.valasz1C);
+            this.Controls.Add(this.next);
+            this.Controls.Add(this.ckeckAnsw);
+            this.Controls.Add(this.valasz3);
+            this.Controls.Add(this.valasz2);
+            this.Controls.Add(this.valasz1);
+            this.Controls.Add(this.kerdes);
+            this.Name = "Form1";
+            this.Text = "Quiz";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label kerdes;
+        private System.Windows.Forms.Label valasz1;
+        private System.Windows.Forms.Label valasz2;
+        private System.Windows.Forms.Label valasz3;
+        private System.Windows.Forms.Button ckeckAnsw;
+        private System.Windows.Forms.Button next;
+        private System.Windows.Forms.RadioButton valasz1C;
+        private System.Windows.Forms.RadioButton valasz2C;
+        private System.Windows.Forms.RadioButton valasz3C;
+    }
+}
+
---
+++ app/Quiz/Quiz/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz
+{
+    public partial class Form1 : Form
+    {
+        private string[] questions = {
+            "Melyik az alapvető osztály a C# programozási nyelvben?",
+            "Mi a C# nyelv egyik fő jellemzője?",
+            "Milyen kulcsszót használunk a C# osztályok öröklődésének deklarálásához?",
+            "Milyen kulcsszót használunk a C# osztályok absztrakt osztályként való deklarálásához?",
+            "Melyik kulcsszót használjuk a C#-ban egy osztály metódusainak és tulajdonságainak elrejtésére?",
+            "Melyik az a függvény, ami automatikusan meghívódik egy osztály példányosításakor?",
+            "Milyen módosítóval rendelkező tagváltozókra nincs közvetlen hozzáférés a leszármazott osztályokból C#-ban?",
+            "Melyik kulcsszót használjuk a C#-ban az interfész definíciójának megadásához?",
+            "Mi az az öröklődési mód, amikor egy osztály több interfészt is implementál?",
+            "Melyik az az attribútum, amely lehetővé teszi az osztály és interfész metadatának kezelését a C# nyelvben?",
+            "Melyik módosítóval rendelkező tagváltozókhoz lehet hozzáférni az adott osztályon kívülről is a C# nyelvben?",
+            "Melyik kulcsszóval tudunk hivatkozni az aktuális osztály egy másik konstruktorára C#-ban?",
+            "Melyik módosítóval rendelkező tagváltozókhoz lehet hozzáférni csak az adott osztályon belül a C# nyelvben?"
+        };
+
+        private string[][] answers = {
+            new string[]{"a) Base", "b) Main", "c) Object"},
+            new string[]{"a) Dinamikus típusú nyelv", "b) Szigorúan típusbiztos nyelv", "c) Logikai nyelv"},
+            new string[]{"a) extend", "b) inherits", "c) extends"},
+            new string[]{"a) abstract", "b) final", "c) sealed"},
+            new string[]{"a) override", "b) private", "c) hide"},
+            new string[]{"a) construct()", "b) init()", "c) constructor()"},
+            new string[]{"a) protected", "b) private", "c) public"},
+            new string[]{"a) interface", "b) implements", "c) extends"},
+            new string[]{"a) Multi-level inheritance", "b) Multiple inheritance", "c) Hierarchical inheritance"},
+            new string[]{"a) [metadata]", "b) [attribute]", "c) [annotation]"},
+            new string[]{"a) private", "b) public", "c) protected"},
+            new string[]{"a) this", "b) super", "c) self"},
+            new string[]{"a) private", "b) protected", "c) public"}
+        };
+
+        private char[] correctAnswers = {
+            'c', 'b', 'b', 'a', 'b', 'c', 'b', 'a', 'b', 'b', 'b', 'a', 'a'
+        };
+        private int point = 0;
+
+        private int currentQuestionIndex = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadQuestion(currentQuestionIndex);
+        }
+
+        private void LoadQuestion(int index)
+        {
+            kerdes.Text = questions[index];
+            valasz1.Text = answers[index][0];
+            valasz2.Text = answers[index][1];
+            valasz3.Text = answers[index][2];
+            valasz1.ForeColor = valasz2.ForeColor = valasz3.ForeColor = Color.Black; // Reset colors
+            valasz1C.Checked = valasz2C.Checked = valasz3C.Checked = false; // Reset checkboxes
+        }
+
+
+
+
+        private void next_Click_1(object sender, EventArgs e)
+        {
+            currentQuestionIndex++;
+            if (currentQuestionIndex < questions.Length)
+            {
+                LoadQuestion(currentQuestionIndex);
+            }
+            else
+            {
+                MessageBox.Show("Quiz Finished!");
+                // Optionally, you can reset the quiz here.
+                currentQuestionIndex = 0;
+                LoadQuestion(currentQuestionIndex);
+            }
+        }
+
+        private void ckeckAnsw_Click(object sender, EventArgs e)
+        {
+            char userAnswer = ' ';
+            if (valasz1C.Checked)
+                userAnswer = 'a';
+            else if (valasz2C.Checked)
+                userAnswer = 'b';
+            else if (valasz3C.Checked)
+                userAnswer = 'c';
+
+            // Highlight correct answer in green
+            switch (correctAnswers[currentQuestionIndex])
+            {
+                case 'a':
+                    valasz1.ForeColor = Color.Green;
+                    break;
+                case 'b':
+                    valasz2.ForeColor = Color.Green;
+                    break;
+                case 'c':
+                    valasz3.ForeColor = Color.Green;
+                    break;
+            }
+
+            // Highlight incorrect answers in red and the user's answer
+            switch (userAnswer)
+            {
+                case 'a':
+                    if (userAnswer != correctAnswers[currentQuestionIndex])
+                        valasz1.ForeColor = Color.Red;
+                    break;
+                case 'b':
+                    if (userAnswer != correctAnswers[currentQuestionIndex])
+                        valasz2.ForeColor = Color.Red;
+                    break;
+                case 'c':
+                    if (userAnswer != correctAnswers[currentQuestionIndex])
+                        valasz3.ForeColor = Color.Red;
+                    break;
+            }
+        }
+
+        
+    }
+}
